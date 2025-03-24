47576f2eb72542f7fe817cf92d46b568005af3a4
desrazer <desrazer@gmail.com>
2025-03-24 17:23:34 +0300

    ДЗ 2.2 PRO

---
+++ lesson-3/script.js
@@ -0,0 +1,7 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 5000;
+let percentage = 20;
+let fullPrice = 100000;
+let responsive = true;
+console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive);
