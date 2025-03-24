1fcb9dc4e73478d461ccf23fc52b6c0fa4862133
desrazer <desrazer@gmail.com>
2025-03-24 17:57:02 +0300

    Обновлен код ДЗ 2.2 PRO

--- lesson-3/script.js
+++ lesson-3/script.js
@@ -6,3 +6,11 @@ let fullPrice = 100000;
 let responsive = true;
 
 console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive);
+
+
+console.log("Название проекта:", titleProject);
+console.log("Типы экранов:", screensValue);
+console.log("Цена за экран:", screenPrice);
+console.log("Процент отката:", percentage);
+console.log("Желаемая сумма прибыли:", fullPrice);
+console.log("Адаптивность:", responsive);
